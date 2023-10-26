73659ba3434aa11bdb49214c32347016d25e6433
Justin Hart <justin@ibotta.com>
2023-10-26 08:38:56 -0700

    merge and bump

--- idiothaus-valheim-backup/src/handlers/scheduled-event-logger.mjs
+++ idiothaus-valheim-backup/src/handlers/scheduled-event-logger.mjs
@@ -1,8 +1,9 @@
-import Client from 'ssh2-sftp-client';
+import * as ftp from "basic-ftp";
 import { Upload } from '@aws-sdk/lib-storage';
 import { S3Client } from '@aws-sdk/client-s3';
 import tmp from 'tmp-promise';
 import * as fs from "fs";
+import { basename } from "path";
 
 /**
  * A Lambda function that logs the payload received from a CloudWatch scheduled event.
@@ -11,45 +12,47 @@ export const scheduledEventLoggerHandler = async (event, context) => {
   // All log statements are written to CloudWatch by default. For more information, see
   // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
   console.info(JSON.stringify(event));
-  const sftp = new Client()
+  const ftps = new ftp.Client()
   const s3 = new S3Client({})
 
   console.log('login to server');
-  await sftp.connect({
-    host: process.env['SFTP_HOST'],
-    port: process.env['SFTP_PORT'],
-    username: process.env['SFTP_USERNAME'],
-    password: process.env['SFTP_PASSWORD'],
+  await ftps.access({
+    host: process.env['FTP_HOST'],
+    port: process.env['FTP_PORT'],
+    user: process.env['FTP_USERNAME'],
+    password: process.env['FTP_PASSWORD'],
+    secure: true,
+    secureOptions: { rejectUnauthorized: false }
   });
 
-  await transfer(sftp, s3, '.db');
-  await transfer(sftp, s3, '.fwl');
+  await transfer(ftps, s3, process.env['FTP_PREFIX'] + '/servermaps/1.mapparts.gzip');
+  await transfer(ftps, s3, process.env['FTP_PREFIX'] + '/worlds/1.world.gzip');
 
-  await sftp.end();
+  await ftps.close();
 };
 
-async function transfer(sftp, s3, ext) {
+async function transfer(ftps, s3, path) {
 
   const tmpfile = await tmp.file();
 
-  console.log('initate download ' + ext + " to " + tmpfile.path);
-  await sftp.fastGet(process.env['SFTP_PATH'] + ext, tmpfile.path, {concurrency: 2});
+  console.log('initate download ' + path + " to " + tmpfile.path);
+  await ftps.downloadTo(tmpfile.path, path);
 
   console.log('initate upload');
-  const parallelUploads3 = new Upload({
+  const uploadS3 = new Upload({
     client: s3,
     params: {
       Bucket: process.env['S3_BUCKET'],
-      Key: process.env['S3_KEY'] + ext,
+      Key: process.env['S3_PREFIX'] + '/' + basename(path),
       Body: fs.createReadStream(tmpfile.path),
     },
   });
 
-  parallelUploads3.on("httpUploadProgress", (progress) => {
+  uploadS3.on("httpUploadProgress", (progress) => {
     console.log(progress);
   });
 
-  await parallelUploads3.done();
+  await uploadS3.done();
   console.log('complete');
 
   tmpfile.cleanup();
